c3232a5c57451e172e9098cefa3bc460117bd103
Ashton Prigge <ashton.prigge@gmail.com>
2021-08-09 10:50:34 -0700

    Initial videojs

--- src/components/DLViewer.vue
+++ src/components/DLViewer.vue
@@ -1,27 +1,27 @@
 <template>
   <div class="dl-viewer">
-    <MejsPlayer v-if="isVideo" :src="src" />
-    <!-- <UniversalViewer
+    <VideoJS v-if="isVideo" :options="videoOptions" />
+    <UniversalViewer
       v-else
       :iiif_manifest="iiif_manifest"
       :iiif_manifest_url="iiif_manifest_url"
       :media="media"
       :uv_config="parseConfig"
-    /> -->
+    />
   </div>
 </template>
 
 <script>
 /* eslint-disable */
 
-import MejsPlayer from "./MejsPlayer.vue";
-// import UniversalViewer from "./UniversalViewer.vue"; // TODO: set up code splitting. Might need to move this?
+import VideoJS from "./VideoJS.vue";
+import UniversalViewer from "./UniversalViewer.vue"; // TODO: set up code splitting. Might need to move this?
 import axios from "axios";
 export default {
   name: "DLViewer",
   components: {
-    MejsPlayer
-    // UniversalViewer
+    VideoJS,
+    UniversalViewer
   },
   props: {
     iiif_manifest_url: {
@@ -45,7 +45,21 @@ export default {
     }
   },
   data() {
-    return { iiif_manifest: {}, media: "", src: "" };
+    return {
+      iiif_manifest: {},
+      media: "",
+      src: "",
+      videoOptions: {
+        // autoplay: true,
+        controls: true,
+        sources: [
+          {
+            src: "https://wowza.library.ucla.edu/iiif_av_public/definst/mp4:synanon/pairtree_root/21/19/8=/zz/00/2h/ds/j2/21198=zz002hdsj2/ark%2B=21198=zz002hdsj2.mp4/manifest.mpd",
+            type: "application/dash+xml"
+          }
+        ]
+      }
+    };
   },
   async beforeCreate() {
     try {
---
+++ src/components/VideoJS.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="video-player">
+    <video
+      ref="videoPlayer"
+      class="video-js"
+      poster="https://static.library.ucla.edu/video_icon.svg"
+    ></video>
+  </div>
+</template>
+
+<script>
+import videojs from "video.js";
+import "video.js/dist/video-js.css";
+
+export default {
+  name: "VideoPlayer",
+  props: {
+    options: {
+      type: Object,
+      default() {
+        return {};
+      },
+    },
+    src: {
+      type: String,
+      default: "",
+    },
+  },
+  data() {
+    return {
+      player: null,
+    };
+  },
+  mounted() {
+    this.player = videojs(
+      this.$refs.videoPlayer,
+      this.options,
+      function onPlayerReady() {
+        // console.log('onPlayerReady', this)
+      }
+    );
+  },
+};
+</script>
+
+<style></style>
