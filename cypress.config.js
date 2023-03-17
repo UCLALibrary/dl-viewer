03d3020b90207a0147afb257395dcbb12104eabe
Andy Wallace <andrewwallace@library.ucla.edu>
2023-03-17 11:15:47 -0700

    wip: switch from weback to vite

---
+++ cypress.config.js
@@ -0,0 +1,9 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  e2e: {
+    specPattern: 'cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}',
+    "baseUrl": "http://localhost:8080",
+    "defaultCommandTimeout": 10000,
+  }
+})
