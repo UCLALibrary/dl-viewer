ec64124997305d61ff66011848d5c707d5786cba
Parinita Mulak <pghorpade@library.ucla.edu>
2023-10-30 15:09:06 -0700

    fix: add logic to use hls for Apple/iOS browsers and dash video for other operating system browsers (#57)
    * fix: add logic to use hls for apple browasers and dash video for other operating system browsers
    
    * tests: fix cypress iframe and videojs poster tests
    
    * Delete cypress/fixtures/example.json
    
    * fix: iframe loading test
    
    ---------
    
    Co-authored-by: Andy Wallace <andrewwallace@library.ucla.edu>

--- cypress/integration/video.spec.js
+++ cypress/integration/video.spec.js
@@ -1,10 +1,18 @@
 describe("A video", () => {
-    it("loads VideoJS", () => {
-      cy.visit("/#?manifest=https%3A%2F%2Fiiif.library.ucla.edu%2Fark%253A%252F21198%252Fzz002hdsj2%2Fmanifest")
-  
-      // Shows track title
-      cy.get(".vjs-poster").should("exist").should("have.css", "background-image", "url(\"https://static.library.ucla.edu/video_icon.svg\")")
-      cy.get(".vjs-big-play-button").should("exist").should("exist")
-    })
-  })
-  
+  it("loads VideoJS", () => {
+    cy.visit(
+      "/#?manifest=https%3A%2F%2Fiiif.library.ucla.edu%2Fark%253A%252F21198%252Fzz002hdsj2%2Fmanifest"
+    );
+
+    // Shows track title
+    cy.get(".vjs-poster").should("be.visible");
+    cy.get(".vjs-poster img")
+      .should("exist")
+      .should(
+        "have.attr",
+        "src",
+        "https://static.library.ucla.edu/video_icon.svg"
+      );
+    cy.get(".vjs-big-play-button").should("exist").should("exist");
+  });
+});
