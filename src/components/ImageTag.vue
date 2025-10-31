e612af348fff7b2c82453f2c01d4060f70de3904
Andy Wallace <andrewwallace@library.ucla.edu>
2025-10-31 11:21:37 -0700

    typescriptify
    use typescript everywhere(except tests). Includes significant refactoring and (notably) packages from https://github.com/iiif-commons

--- src/components/ImageTag.vue
+++ src/components/ImageTag.vue
@@ -1,21 +1,33 @@
 <template>
-  <img v-if="options.src" v-bind="options" class="image-tag" />
+  <img
+    v-if="iiif_manifest"
+    :src="src"
+    :height="height"
+    :width="width"
+    :alt="alt"
+    class="image-tag"
+  />
   <div v-else class="image-tag">
     <!-- CSS Placeholder w/o broken image icon -->
   </div>
 </template>
 
-<script>
-export default {
-  name: 'ImageTag',
-  props: {
-    options: {
-      // { src: [...] }
-      type: Object,
-      required: true,
-    },
+<script setup lang="ts">
+import _get from 'lodash/get'
+import { computed } from 'vue'
+
+const props = defineProps({
+  iiif_manifest: {
+    // { src: [...] }
+    type: Object,
+    required: true,
   },
-}
+})
+
+const src = computed(() => _get(props.iiif_manifest, 'items[0].items[0].items[0].body.id'))
+const height = computed(() => _get(props.iiif_manifest, 'items[0].items[0].items[0].body.height'))
+const width = computed(() => _get(props.iiif_manifest, 'items[0].items[0].items[0].body.width'))
+const alt = computed(() => _get(props.iiif_manifest, 'label.none[0]'))
 </script>
 
 <style>
