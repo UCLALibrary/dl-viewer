d16c0951bb60a919a1de4a88a1e05d466bd6ef34
Andy Wallace <andrewwallace@library.ucla.edu>
2025-11-04 14:48:05 -0800

    Load UV 4 directly, not in iframe

--- cypress/e2e/collection.spec.js
+++ cypress/e2e/collection.spec.js
@@ -1,19 +1,8 @@
 describe('A IIIF Collection', () => {
-  const URL_PARAMETERS = '#?manifest=https%3A%2F%2Fiiif.library.ucla.edu%2Fcollections%2Fark%253A%252F21198%252Fz11c574k';
+  const URL_PARAMETERS =
+    '#?manifest=https%3A%2F%2Fiiif.library.ucla.edu%2Fcollections%2Fark%253A%252F21198%252Fz11c574k'
 
-    it('loads Universal Viewer in an iframe', () => {
-      cy.visit('/' + URL_PARAMETERS)
-
-      // UV loads inside an iframe
-      cy.frameLoaded("#universalviewer-iframe", {
-        url: '/uv.html' + URL_PARAMETERS,
-      });
-    })
-  
-
-    it('loads in Universal Viewer!', () => {
-      cy.visit('/uv.html' + URL_PARAMETERS)
-
-    })
+  it('loads in Universal Viewer!', () => {
+    cy.visit('/' + URL_PARAMETERS)
   })
-  
+})
